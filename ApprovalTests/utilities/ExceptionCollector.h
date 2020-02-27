fe0357080f5c43dc1d61861fd3a6ab347e43c7d7
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-27 17:15:48 +0000

    R!! Out-of-line ExceptionCollector methods

--- ApprovalTests/utilities/ExceptionCollector.h
+++ ApprovalTests/utilities/ExceptionCollector.h
@@ -1,8 +1,6 @@
 #ifndef APPROVALTESTS_CPP_EXCEPTIONCOLLECTOR_H
 #define APPROVALTESTS_CPP_EXCEPTIONCOLLECTOR_H
 
-#include <sstream>
-#include <exception>
 #include <string>
 #include <vector>
 #include <functional>
@@ -14,43 +12,10 @@ namespace ApprovalTests
         std::vector<std::string> exceptionMessages;
 
     public:
-        void gather(std::function<void(void)> functionThatThrows)
-        {
-            try
-            {
-                functionThatThrows();
-            }
-            catch (const std::exception& e)
-            {
-                exceptionMessages.emplace_back(e.what());
-            }
-        }
-        ~ExceptionCollector()
-        {
-            if (!exceptionMessages.empty())
-            {
-                exceptionMessages.emplace_back(
-                    "ERROR: Calling code forgot to call "
-                    "exceptionCollector.release()");
-            }
-            release();
-        }
+        void gather(std::function<void(void)> functionThatThrows);
+        ~ExceptionCollector();
 
-        void release()
-        {
-            if (!exceptionMessages.empty())
-            {
-                std::stringstream s;
-                s << exceptionMessages.size() << " exceptions were thrown:\n\n";
-                int count = 1;
-                for (const auto& error : exceptionMessages)
-                {
-                    s << count++ << ") " << error << '\n';
-                }
-                exceptionMessages.clear();
-                throw std::runtime_error(s.str());
-            }
-        }
+        void release();
     };
 }
 
