018f48e2d779553502a7799543ba7b61cb943c0d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-27 18:19:04 +0000

    R!! Put all implementations inside 'namespace ApprovalTests'

--- ApprovalTests/core/FileApprover.cpp
+++ ApprovalTests/core/FileApprover.cpp
@@ -2,76 +2,79 @@
 
 #include "ApprovalException.h"
 #include "ApprovalTests/core/ApprovalNamer.h"
-#include "ApprovalTests/reporters/FrontLoadedReporterDisposer.h"
 #include "ApprovalTests/comparators/ComparatorFactory.h"
 #include "ApprovalTests/core/ApprovalWriter.h"
 #include "ApprovalTests/reporters/FrontLoadedReporterFactory.h"
 #include "ApprovalTests/utilities/FileUtils.h"
 
-ApprovalTests::ComparatorDisposer
-ApprovalTests::FileApprover::registerComparatorForExtension(
-    const std::string& extensionWithDot,
-    std::shared_ptr<ApprovalComparator> comparator)
+namespace ApprovalTests
 {
-    return ComparatorFactory::registerComparator(extensionWithDot, comparator);
-}
-
-void ApprovalTests::FileApprover::verify(
-    const std::string& receivedPath,
-    const std::string& approvedPath,
-    const ApprovalTests::ApprovalComparator& comparator)
-{
-    if (!FileUtils::fileExists(approvedPath))
+    ComparatorDisposer FileApprover::registerComparatorForExtension(
+        const std::string& extensionWithDot,
+        std::shared_ptr<ApprovalComparator> comparator)
     {
-        throw ApprovalMissingException(receivedPath, approvedPath);
+        return ComparatorFactory::registerComparator(extensionWithDot,
+                                                     comparator);
     }
 
-    if (!FileUtils::fileExists(receivedPath))
+    void FileApprover::verify(const std::string& receivedPath,
+                              const std::string& approvedPath,
+                              const ApprovalComparator& comparator)
     {
-        throw ApprovalMissingException(approvedPath, receivedPath);
+        if (!FileUtils::fileExists(approvedPath))
+        {
+            throw ApprovalMissingException(receivedPath, approvedPath);
+        }
+
+        if (!FileUtils::fileExists(receivedPath))
+        {
+            throw ApprovalMissingException(approvedPath, receivedPath);
+        }
+
+        if (!comparator.contentsAreEquivalent(receivedPath, approvedPath))
+        {
+            throw ApprovalMismatchException(receivedPath, approvedPath);
+        }
     }
 
-    if (!comparator.contentsAreEquivalent(receivedPath, approvedPath))
+    void FileApprover::verify(const std::string& receivedPath,
+                              const std::string& approvedPath)
     {
-        throw ApprovalMismatchException(receivedPath, approvedPath);
+        verify(receivedPath,
+               approvedPath,
+               *ComparatorFactory::getComparatorForFile(receivedPath));
     }
-}
 
-void ApprovalTests::FileApprover::verify(const std::string& receivedPath,
-                                         const std::string& approvedPath)
-{
-    verify(receivedPath,
-           approvedPath,
-           *ComparatorFactory::getComparatorForFile(receivedPath));
-}
-
-void ApprovalTests::FileApprover::verify(const ApprovalTests::ApprovalNamer& n,
-                                         const ApprovalTests::ApprovalWriter& s,
-                                         const ApprovalTests::Reporter& r)
-{
-    std::string approvedPath = n.getApprovedFile(s.getFileExtensionWithDot());
-    std::string receivedPath = n.getReceivedFile(s.getFileExtensionWithDot());
-    s.write(receivedPath);
-    try
+    void FileApprover::verify(const ApprovalNamer& n,
+                              const ApprovalWriter& s,
+                              const Reporter& r)
     {
-        verify(receivedPath, approvedPath);
-        s.cleanUpReceived(receivedPath);
+        std::string approvedPath =
+            n.getApprovedFile(s.getFileExtensionWithDot());
+        std::string receivedPath =
+            n.getReceivedFile(s.getFileExtensionWithDot());
+        s.write(receivedPath);
+        try
+        {
+            verify(receivedPath, approvedPath);
+            s.cleanUpReceived(receivedPath);
+        }
+        catch (const ApprovalException&)
+        {
+            reportAfterTryingFrontLoadedReporter(receivedPath, approvedPath, r);
+            throw;
+        }
     }
-    catch (const ApprovalException&)
-    {
-        reportAfterTryingFrontLoadedReporter(receivedPath, approvedPath, r);
-        throw;
-    }
-}
 
-void ApprovalTests::FileApprover::reportAfterTryingFrontLoadedReporter(
-    const std::string& receivedPath,
-    const std::string& approvedPath,
-    const ApprovalTests::Reporter& r)
-{
-    auto tryFirst = FrontLoadedReporterFactory::getFrontLoadedReporter();
-    if (!tryFirst->report(receivedPath, approvedPath))
+    void FileApprover::reportAfterTryingFrontLoadedReporter(
+        const std::string& receivedPath,
+        const std::string& approvedPath,
+        const Reporter& r)
     {
-        r.report(receivedPath, approvedPath);
+        auto tryFirst = FrontLoadedReporterFactory::getFrontLoadedReporter();
+        if (!tryFirst->report(receivedPath, approvedPath))
+        {
+            r.report(receivedPath, approvedPath);
+        }
     }
 }
