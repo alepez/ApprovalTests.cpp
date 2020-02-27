93b6dd7f9242c88c83dde3187377fc5d15d52ad6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-27 18:02:39 +0000

    R!! Out-of-line GenericDiffReporter methods

---
+++ ApprovalTests/reporters/GenericDiffReporter.cpp
@@ -0,0 +1,13 @@
+#include "GenericDiffReporter.h"
+
+namespace ApprovalTests
+{
+    GenericDiffReporter::GenericDiffReporter(const std::string& program)
+        : CommandReporter(program, &launcher)
+    {
+    }
+    GenericDiffReporter::GenericDiffReporter(const DiffInfo& info)
+        : CommandReporter(info.getProgramForOs(), info.arguments, &launcher)
+    {
+    }
+}
--- ApprovalTests/reporters/GenericDiffReporter.h
+++ ApprovalTests/reporters/GenericDiffReporter.h
@@ -1,11 +1,9 @@
 #ifndef APPROVALTESTS_CPP_GENERICDIFFREPORTER_H
 #define APPROVALTESTS_CPP_GENERICDIFFREPORTER_H
 
-#include "DiffPrograms.h"
 #include "CommandReporter.h"
 #include "ApprovalTests/launchers/SystemLauncher.h"
 #include <string>
-#include <vector>
 
 namespace ApprovalTests
 {
@@ -15,15 +13,9 @@ namespace ApprovalTests
         SystemLauncher launcher;
 
     public:
-        explicit GenericDiffReporter(const std::string& program)
-            : CommandReporter(program, &launcher)
-        {
-        }
+        explicit GenericDiffReporter(const std::string& program);
 
-        explicit GenericDiffReporter(const DiffInfo& info)
-            : CommandReporter(info.getProgramForOs(), info.arguments, &launcher)
-        {
-        }
+        explicit GenericDiffReporter(const DiffInfo& info);
     };
 }
 
