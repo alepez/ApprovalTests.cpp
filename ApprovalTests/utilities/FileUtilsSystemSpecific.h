4cf4ac6b31d58a9f0b8c1ae632b269ded29bb504
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-27 16:29:10 +0000

    R!! Out-of-line FileUtilsSystemSpecific methods

--- ApprovalTests/utilities/FileUtilsSystemSpecific.h
+++ ApprovalTests/utilities/FileUtilsSystemSpecific.h
@@ -1,7 +1,7 @@
 #ifndef APPROVALTESTS_CPP_FILEUTILSSYSTEMSPECIFIC_H
 #define APPROVALTESTS_CPP_FILEUTILSSYSTEMSPECIFIC_H
 
-#include "SystemUtils.h"
+#include <string>
 
 namespace ApprovalTests
 {
@@ -10,27 +10,10 @@ namespace ApprovalTests
     public:
         static std::string getCommandLineForCopy(const std::string& source,
                                                  const std::string& destination,
-                                                 bool isWindows)
-        {
-            if (isWindows)
-            {
-                return std::string("copy /Y ") + "\"" + source + "\" \"" +
-                       destination + "\"";
-            }
-            else
-            {
-                return std::string("cp ") + "\"" + source + "\" \"" +
-                       destination + "\"";
-            }
-        }
+                                                 bool isWindows);
 
         static void copyFile(const std::string& source,
-                             const std::string& destination)
-        {
-            auto cmd = getCommandLineForCopy(
-                source, destination, SystemUtils::isWindowsOs());
-            SystemUtils::runSystemCommandOrThrow(cmd);
-        }
+                             const std::string& destination);
     };
 }
 #endif
